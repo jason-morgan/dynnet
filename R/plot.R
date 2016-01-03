67cbd44293ddb531a18f72e75e0a4cfeb3b2f9ad
Jason W. Morgan <jwm@skepsi.net>
2016-01-03 16:11:48 -0500

    Fixed plot.dynnet layout

--- R/plot.R
+++ R/plot.R
@@ -1,17 +1,21 @@
-plot.dynnet <- function(network, ...)
+plot.dynnet <- function(network, layout=NULL, ...)
 {
     g <- graphs(network)
     t <- length(g)
 
-    if (t <= 15) {
-        r <- ceiling(t/5)
-        c <- ceiling(t/r)
-    } else {
-        r <- ceiling(15/5)
-        c <- ceiling(15/r)
+    if (is.null(layout))
+    {
+        if (t <= 15) {
+            r <- ceiling(t/5)
+            c <- ceiling(t/r)
+        } else {
+            r <- ceiling(15/5)
+            c <- ceiling(15/r)
+        }
+        layout <- c(r, c)
     }
 
-    par(mfrow=c(r, c))
+    par(mfrow=layout)
     for (i in seq_along(g))
         plot(g[[i]], ...)
 }
