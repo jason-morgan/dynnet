e8a43cd9a3d4113fc53cb7f3f28c28dbba28fe8f
Jason Morgan <jwm@aeryn.skepsi.net>
2016-12-26 11:57:48 -0500

    Added copyright, documentation, cleanup

---
+++ R/dynnet-package.R
@@ -0,0 +1,53 @@
+## ----------------------------------------------------------------------------
+## This file is part of dynnet
+##
+## Copyright (C) 2016 Jason W. Morgan <jason.w.morgan@gmail.com>
+##
+## dynnet and is free software: you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation, either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## ----------------------------------------------------------------------------
+
+
+##' @useDynLib dynnet
+NULL
+
+##'
+##' \pkg{dynnet} will provide a set of latent space models for static and
+##' dynamic networks. Currently, the package provides the original Euclidean
+##' model of Hoff, Raftery, & Handcock (HRH; 2002) along with a variation of the
+##' model that uses reference nodes as a method of parameter
+##' identification. Extensions to the Projection Model and dynamic networks is
+##' planned for the near future.
+##'
+##' \tabular{ll}{
+##' Package: \tab dynnet \cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.0.900\cr
+##' Date: \tab 2016-12-26\cr
+##' License: \tab GPL-3 \cr
+##' LazyLoad: \tab yes\cr
+##' }
+##'
+##' @name dynnet-package
+##' @aliases dynnet
+##' @docType package
+##' @title Latent Space Models for Static and Dynamic Networks
+##' @note \pkg{dynnet} is developed by Jason W. Morgan.
+##' @author Jason W. Morgan (\email{jason.w.morgan@@gmail.com})
+##'
+##' @references Hoff, Peter D., Adrian E. Raftery, & Mark S. Handcock (2002)
+##' ``Latent Space Approaches to Social Network Analysis'', \emph{Journal of the
+##' American Statistical Association}, 97(460), 1090-1098.
+##' @keywords package
+NULL
--- R/dynnet.R
+++ R/dynnet.R
@@ -1,15 +1,23 @@
-##' @useDynLib dynnet
-NULL
+## ----------------------------------------------------------------------------
+## This file is part of dynnet
+##
+## Copyright (C) 2016 Jason W. Morgan <jason.w.morgan@gmail.com>
+##
+## dynnet and is free software: you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation, either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## ----------------------------------------------------------------------------
 
-##' Latent Space Models for Static and Dynamic Networks
-##'
-##' \pkg{dynnet} will provide a set of latent space models for static and
-##' dynamic networks. Currently, the package provides the original Euclidean
-##' model of Hoff, Raftery, & Handcock (HRH; 2002) along with a variation of the
-##' model that uses reference nodes as a method of parameter
-##' identification. Extensions to the Projection Model and dynamic networks is
-##' planned for the near future.
-"_DYNNET"
 
 ##' Generate a dynamic network object from a list of adjacency matrices.
 ##'
