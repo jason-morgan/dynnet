a790baabb728cd0a2ec2b9ee6e7ac1f0a332c9c8
Jason Morgan <jwm@aeryn.skepsi.net>
2017-01-05 21:55:21 -0500

    Added basic testing script

---
+++ tests/dev/basic.R
@@ -0,0 +1,39 @@
+## =============================================================================
+## Testing during development
+## =============================================================================
+
+options(width=120)
+
+library(devtools)
+library(lattice)
+library(coda)
+library(igraph)
+setwd("~/lib/R/dynnet")
+document("~/lib/R/dynnet")
+load_all("~/lib/R/dynnet")
+
+data(florentine, package="ergm")
+(Flo  <- to_dynnet(flomarriage))
+
+## -----------------------------------------------------------------------------
+## Without reference units
+## -----------------------------------------------------------------------------
+
+## tweak for better mixing
+ctl <- control.lsm(MCMC.burnin=2^16, MCMC.interval=100)
+
+document("~/lib/R/dynnet")
+model0 <- lsm(Flo ~ 1, ref=NULL, d=2, seed=1234)
+model1 <- lsm(Flo ~ 1, ref=NULL, d=2, seed=1234, method="MH", control=ctl)
+
+## -----------------------------------------------------------------------------
+## With reference units
+## -----------------------------------------------------------------------------
+
+ref <- list(pos=matrix(c(-0.50, -0.50,
+                          0.00,  0.50,
+                          0.50, -0.50), ncol=2, byrow=TRUE),
+            idx=c(7, 9, 14))
+
+model2 <- lsm(Flo ~ 1, ref=ref, d=2, seed=1234)
+model3 <- lsm(Flo ~ 1, ref=ref, d=2, seed=1234, method="MH", control=ctl)
